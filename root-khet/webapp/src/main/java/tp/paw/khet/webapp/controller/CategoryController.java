c5149bae29c659119a76095315a513e80cff3f82
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2017-04-13 20:06:06 -0300

    CaseInsensitive de de Category en la url

--- root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/CategoryController.java
+++ root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/CategoryController.java
@@ -2,11 +2,14 @@ package tp.paw.khet.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import tp.paw.khet.Category;
 import tp.paw.khet.service.ProductService;
+import tp.paw.khet.webapp.utils.CaseInsensitiveConverter;
 
 @Controller
 public class CategoryController {
@@ -15,10 +18,14 @@ public class CategoryController {
     private ProductService productService;
 	
 	@RequestMapping(value = "/category/{category}")
-	public ModelAndView showProductsForCategory(@PathVariable(value = "category") final Category category) {
+	public ModelAndView showProductsForCategory(@PathVariable(value = "category") Category category) {
 		 ModelAndView mav = new ModelAndView("category");
 		 mav.addObject("products",productService.getProductsByCategory(category));
 		 return mav;
 	}
 
+	@InitBinder
+	 public void initBinder(WebDataBinder binder) {
+	  binder.registerCustomEditor(Category.class,new CaseInsensitiveConverter<>(Category.class));
+	 }
 }
