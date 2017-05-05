a4b4a4624f5cf901cfca635046fd8bb37e201238
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2017-05-04 22:53:57 -0300

    Ahora funciona cambiando las categorias

--- root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/CategoryController.java
+++ root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/CategoryController.java
@@ -1,13 +1,17 @@
 package tp.paw.khet.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import tp.paw.khet.Category;
+import tp.paw.khet.User;
+import tp.paw.khet.controller.auth.SecurityUserService;
 import tp.paw.khet.service.ProductService;
 import tp.paw.khet.webapp.utils.CaseInsensitiveConverter;
 
@@ -16,6 +20,14 @@ public class CategoryController {
 
 	@Autowired
     private ProductService productService;
+	
+	@Autowired
+	private SecurityUserService securityUserService;
+	
+	@ModelAttribute("loggedUser")
+	public User loggedUser() {
+		return securityUserService.getLoggedInUser();
+	}
 	
 	@RequestMapping(value = "/category/{category}")
 	public ModelAndView showProductsForCategory(@PathVariable(value = "category") Category category) {
@@ -29,4 +41,5 @@ public class CategoryController {
 	 public void initBinder(WebDataBinder binder) {
 	  binder.registerCustomEditor(Category.class,new CaseInsensitiveConverter<>(Category.class));
 	 }
+
 }
