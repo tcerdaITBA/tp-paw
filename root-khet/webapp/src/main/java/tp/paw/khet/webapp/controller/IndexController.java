b6b022bef747b29b7c28ad7faaa5914eaf9571cf
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2017-05-04 21:16:01 -0300

    Fotito en navBar cuando esta loggeado

--- root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/IndexController.java
+++ root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/IndexController.java
@@ -3,19 +3,34 @@ package tp.paw.khet.webapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import tp.paw.khet.Category;
+import tp.paw.khet.User;
+import tp.paw.khet.controller.auth.SecurityUserService;
 import tp.paw.khet.service.ProductService;
+import tp.paw.khet.service.UserService;
 
 @Controller
 public class IndexController {
 		
     @Autowired
     private ProductService productService;
+    
+    @Autowired 
+    private UserService userService;
+    
+	@Autowired
+	private SecurityUserService securityUserService;
+    
+	@ModelAttribute("loggedUser")
+	public User loggedUser() {
+		return securityUserService.getLoggedInUser();
+	}
     
 	@RequestMapping("/")
 	public ModelAndView index() {
@@ -29,6 +44,12 @@ public class IndexController {
 	@RequestMapping(value = "/product/{productId}/logo", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
 	public byte[] deliverLogo(@PathVariable(value = "productId") int productId) {
 		return productService.getLogoByProductId(productId);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/user/{userId}/profilePicture", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
+	public byte[] deliverProfilePicture(@PathVariable(value = "userId") int userId) {
+		return userService.getProfilePictureByUserId(userId);
 	}
 	
 	@RequestMapping("/login")
