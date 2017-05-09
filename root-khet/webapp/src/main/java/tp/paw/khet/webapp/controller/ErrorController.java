39e73b7331383e67753784aa18623e367ffd54e5
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2017-05-09 16:34:48 -0300

    Error Controller (Falta jsp bien hecho)

---
+++ root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/ErrorController.java
@@ -0,0 +1,15 @@
+package tp.paw.khet.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ErrorController {
+
+	@RequestMapping("/403")
+	public ModelAndView forbidden() {
+		return new ModelAndView("403");
+	}
+	
+}
---
+++ root-khet/webapp/src/main/webapp/WEB-INF/jsp/403.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt"%>
+
+<html>
+	<head>
+
+	</head>
+	<body>
+	<h1>403</h1>
+	</body>
+
+</html>
