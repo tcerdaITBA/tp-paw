603872e07d7908f2bcb2b05ac2d50482d1a1fe52
tcerdaITBA <tcerda@itba.edu.ar>
2017-05-15 19:03:42 -0300

    Quitando cosas de IndexController y llevando a ProfileController

--- root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/IndexController.java
+++ root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/IndexController.java
@@ -1,34 +1,23 @@
 package tp.paw.khet.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import tp.paw.khet.Category;
-import tp.paw.khet.Product;
 import tp.paw.khet.User;
 import tp.paw.khet.controller.auth.SecurityUserService;
 import tp.paw.khet.service.ProductService;
-import tp.paw.khet.webapp.exception.ForbiddenException;
 import tp.paw.khet.webapp.exception.ResourceNotFoundException;
-import tp.paw.khet.webapp.exception.UnauthorizedException;
-import tp.paw.khet.service.UserService;
 
 @Controller
 public class IndexController {
 		
     @Autowired
     private ProductService productService;
-    
-    @Autowired 
-    private UserService userService;
     
 	@Autowired
 	private SecurityUserService securityUserService;
@@ -54,64 +43,9 @@ public class IndexController {
 		mav.addObject("totalPages", maxPage);
 		return mav;
 	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/product/{productId}/logo", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
-	public byte[] deliverLogo(@PathVariable(value = "productId") int productId) {
-		return productService.getLogoByProductId(productId);
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/profile/{userId}/profilePicture", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
-	public byte[] deliverProfilePicture(@PathVariable(value = "userId") int userId) {
-		return userService.getProfilePictureByUserId(userId);
-	}
-	
+		
 	@RequestMapping("/login")
 	public ModelAndView login() {
 		return new ModelAndView("login");
 	}
-	
-	@RequestMapping("/profile/{userId}")
-	public ModelAndView user(@PathVariable final int userId) throws ResourceNotFoundException {
-		ModelAndView mav = new ModelAndView("profile");
-		
-		User user = userService.getUserById(userId);
-		
-		if (user == null)
-			throw new ResourceNotFoundException();
-		
-		User loggedUser = securityUserService.getLoggedInUser();
-		
-		mav.addObject("loggedUser", loggedUser);
-		mav.addObject("profileUser", user);
-		mav.addObject("us", userService.getUserById(userId));
-		mav.addObject("products", productService.getPlainProductsByUserId(userId));
-		return mav;
-	}
-	
-	@RequestMapping(value = "/delete/product/{productId}", method = RequestMethod.POST)
-	public ModelAndView deleteProduct(@PathVariable final int productId)
-	throws ResourceNotFoundException, UnauthorizedException, ForbiddenException{
-		Product product = productService.getFullProductById(productId);
-		
-		if (product == null) 
-			throw new ResourceNotFoundException();
-		
-		User loggedUser = securityUserService.getLoggedInUser();
-		User productCreator = product.getCreator();
-		
-		if (loggedUser == null)
-			throw new UnauthorizedException();
-		
-		if (loggedUser.getUserId() != productCreator.getUserId())
-				throw new ForbiddenException();
-	
-		productService.deleteProductById(productId);
-		
-		User owner = product.getCreator();
-		
-		return new ModelAndView("redirect:/profile/" + owner.getUserId());
-	}
-	
 }
