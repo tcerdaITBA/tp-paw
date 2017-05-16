75b764f4d3e8fc5195652e259c27100c0095fe98
tcerdaITBA <tcerda@itba.edu.ar>
2017-05-15 23:17:48 -0300

    JavaDocs de los metodos de modificación de usuario

--- root-khet/interface/src/main/java/tp/paw/khet/persistence/UserDao.java
+++ root-khet/interface/src/main/java/tp/paw/khet/persistence/UserDao.java
@@ -37,8 +37,20 @@ public interface UserDao {
 	 */
 	public byte[] getProfilePictureByUserId(int userId);
 	
+	/**
+	 * Changes an [@link User] password
+	 * @param userId - ID of the user
+	 * @param password - New user's password
+	 * @return The user with the modified password or null if user doesn't exist
+	 */
 	public User changePassword(int userId, String password);
 	
+	/**
+	 * Changes an {@link User} profile picture
+	 * @param userId - ID of the user
+	 * @param profilePicture - Byte array with the new profile picture
+	 * @return The user with the given ID
+	 */
 	public User changeProfilePicture(int userId, byte[] profilePicture);
 	
 }
