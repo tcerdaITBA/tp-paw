191f521dca6705a7ae416d0a202f0677c6b5917b
tcerdaITBA <tcerda@itba.edu.ar>
2017-05-31 11:23:01 -0300

    Chequeo de not null en cambio de foto de perfil

--- root-khet/webapp/src/main/java/tp/paw/khet/webapp/form/FormChangePicture.java
+++ root-khet/webapp/src/main/java/tp/paw/khet/webapp/form/FormChangePicture.java
@@ -3,6 +3,8 @@ package tp.paw.khet.webapp.form;
 import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.sun.istack.internal.NotNull;
+
 import tp.paw.khet.webapp.form.constraints.FileMediaType;
 import tp.paw.khet.webapp.form.constraints.FileSize;
 
@@ -10,6 +12,7 @@ public class FormChangePicture {
 
 	@FileMediaType({MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})	
 	@FileSize(min = 1)
+	@NotNull
 	private MultipartFile profilePicture;
 
 	public MultipartFile getProfilePicture() {
