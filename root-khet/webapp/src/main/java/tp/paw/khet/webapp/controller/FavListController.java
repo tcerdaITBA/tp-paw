19bd49aea5239b6c49155712778ed6f3e16ec171
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2017-06-15 13:35:08 -0300

    View FavListController

--- root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/FavListController.java
+++ root-khet/webapp/src/main/java/tp/paw/khet/webapp/controller/FavListController.java
@@ -8,14 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import tp.paw.khet.model.FavList;
 import tp.paw.khet.model.User;
 import tp.paw.khet.service.FavListService;
+import tp.paw.khet.webapp.exception.FavListNotFoundException;
 import tp.paw.khet.webapp.form.FormFavList;
 
 @Controller
@@ -50,5 +53,25 @@ public class FavListController {
 	private void setErrorState(FormFavList favListForm, BindingResult errors, RedirectAttributes attr) {
 		attr.addFlashAttribute("org.springframework.validation.BindingResult.favListForm", errors);
 		attr.addFlashAttribute("createFavListForm", favListForm);		
+	}
+	
+	@RequestMapping(value="/favlist/{favListId}")
+	public ModelAndView viewFavList(@PathVariable final int favListId) throws FavListNotFoundException{
+		LOGGER.debug("Accessed user favList with ID: {}", favListId);
+		
+		final ModelAndView mav = new ModelAndView("favList");
+		final FavList favList = favListService.getFavListById(favListId);
+		
+		if(favList == null) {
+			LOGGER.warn("Cannot render favList: favlist ID not found: {}", favListId);
+			throw new FavListNotFoundException();
+		}
+		
+		mav.addObject("favlist", favList.getProductList());
+		mav.addObject("favlistName", favList.getName());
+		mav.addObject("creator", favList.getCreator());
+		
+		return mav;
+
 	}
 }
