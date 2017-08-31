215f1f87126f894e5cee13c0251bd047c3e4d98c
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2017-08-31 10:56:57 -0300

    Arreglado lo del name

--- generator/app/scripts/controllers/DeleteModalCtrl.js
+++ generator/app/scripts/controllers/DeleteModalCtrl.js
@@ -2,6 +2,8 @@ define(['productSeek'], function(productSeek) {
 'use strict';
     productSeek.controller('DeleteModalCtrl', ['$scope', 'product', function($scope, product) {
         
+        $scope.product = product;
+
         $scope.cancel = function(){
             console.log(product);
             $scope.$dismiss();
