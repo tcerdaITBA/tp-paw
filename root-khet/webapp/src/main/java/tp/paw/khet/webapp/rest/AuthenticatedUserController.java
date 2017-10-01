ad312d3fa19ef5d1d5bf2637bdc53ca85a5c3e77
tcerdaITBA <tcerda@itba.edu.ar>
2017-10-01 13:13:28 -0300

    API cambiar imagen de perfil

--- root-khet/webapp/src/main/java/tp/paw/khet/webapp/rest/AuthenticatedUserController.java
+++ root-khet/webapp/src/main/java/tp/paw/khet/webapp/rest/AuthenticatedUserController.java
@@ -1,5 +1,7 @@
 package tp.paw.khet.webapp.rest;
 
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -17,9 +19,11 @@ import org.springframework.stereotype.Controller;
 
 import tp.paw.khet.controller.auth.SecurityUserService;
 import tp.paw.khet.model.User;
+import tp.paw.khet.service.UserService;
 import tp.paw.khet.webapp.dto.ExceptionDTO;
 import tp.paw.khet.webapp.dto.UserDTO;
 import tp.paw.khet.webapp.dto.form.FormChangePassword;
+import tp.paw.khet.webapp.dto.form.FormPicture;
 import tp.paw.khet.webapp.exception.DTOValidationException;
 import tp.paw.khet.webapp.exceptionmapper.ValidationMapper;
 import tp.paw.khet.webapp.validators.DTOConstraintValidator;
@@ -36,6 +40,9 @@ public class AuthenticatedUserController {
 	
 	@Autowired
 	private SecurityUserService securityUserService;
+	
+	@Autowired
+	private UserService userService;
 	
 	@Autowired
 	private DTOConstraintValidator DTOValidator;
@@ -55,8 +62,9 @@ public class AuthenticatedUserController {
 	
 	@PUT
 	@Path("/password")
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response changePassword(final FormChangePassword changePasswordForm) throws DTOValidationException {
-		LOGGER.debug("Accessed update password");
+		LOGGER.debug("Accessed change password");
 		
 		DTOValidator.validate(changePasswordForm, "Failed to validate change password form");
 		
@@ -66,6 +74,21 @@ public class AuthenticatedUserController {
 			return Response.status(ValidationMapper.UNPROCESSABLE_ENTITY).entity(new ExceptionDTO("Incorrect password")).build();
 		
 		securityUserService.changePassword(authenticatedUser.getUserId(), changePasswordForm.getNewPassword());
+		
+		return Response.noContent().build();
+	}
+	
+	@PUT
+	@Path("/picture")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response changePicture(@BeanParam final FormPicture picture) throws DTOValidationException {
+		LOGGER.debug("Accessed change picture");
+		
+		DTOValidator.validate(picture, "Failed to validate picture");
+
+		final User authenticatedUser = securityUserService.getLoggedInUser();
+
+		userService.changeProfilePicture(authenticatedUser.getUserId(), picture.getPictureBytes());
 		
 		return Response.noContent().build();
 	}
