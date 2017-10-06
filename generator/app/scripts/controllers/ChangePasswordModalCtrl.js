c9d84eeef7a0adb1f550e129994ceaaa67d79a50
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2017-10-06 18:30:38 -0300

    ChangePassword en Modal
    Faltan validaciones

--- generator/app/scripts/controllers/ChangePasswordModalCtrl.js
+++ generator/app/scripts/controllers/ChangePasswordModalCtrl.js
@@ -1,7 +1,7 @@
-define(['productSeek', 'services/authService', 'services/sessionService'], function(productSeek) {
+define(['productSeek', 'services/authService', 'services/sessionService', 'services/restService'], function(productSeek) {
 
     'use strict';
-    productSeek.controller('ChangePasswordModalCtrl', ['authService', 'sessionService', '$scope', function(auth,session, $scope) {
+    productSeek.controller('ChangePasswordModalCtrl', ['authService', 'sessionService', 'restService', '$scope', function(auth,session,restService, $scope) {
         $scope.passwordForm = {};
         $scope.passwordForm.oldPassword = {};
         $scope.passwordForm.newPassword = {};
@@ -9,9 +9,15 @@ define(['productSeek', 'services/authService', 'services/sessionService'], funct
         
         $scope.passwordSubmit = function() {
             console.log($scope.passwordForm);
-            var user = session.getUser();
-            // user.password = $scope.passwordForm.newPassword.text;
-			
+            //Falta validación de newPassword = confirmPassword
+            oldPass = $scope.passwordForm.oldPassword.text;
+            newPass = $scope.passwordForm.newPassword.text;
+            confirm = $scope.passwordForm.confirmPassword.text;
+            if(newPass === confirm){
+                restService.changePassword(oldPass,newPass);
+            } else {
+                //Validaciones en caso de que las contraseñas ingresadas sean distintas
+            }
         };
         
         $scope.cancel = function(){
