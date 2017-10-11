a53d460666a01f93d019a4b51ab3e79b353a55db
Luciano Bianchi <lucianogbianchi@gmail.com>
2017-10-11 00:32:00 -0300

    Logo en postForm

--- generator/app/scripts/controllers/PostCtrl.js
+++ generator/app/scripts/controllers/PostCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
-define(['productSeek', 'directives/ngFileRead'], function(productSeek) {
+define(['productSeek', 'directives/ngFileRead', 'services/restService'], function(productSeek) {
 
-	productSeek.controller('PostCtrl', ['$scope', 'categories', 'productImagesCount', 'productVideosCount', function($scope, categories, productImagesCount, productVideosCount) {
+	productSeek.controller('PostCtrl', ['$scope', 'categories', 'productImagesCount', 'productVideosCount', 'restService', function($scope, categories, productImagesCount, productVideosCount, restService) {
 		$scope.categories = categories;
 		
 		$scope.youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
@@ -9,7 +9,7 @@ define(['productSeek', 'directives/ngFileRead'], function(productSeek) {
 		$scope.product = {};
 		$scope.product.category = $scope.categories[categories.indexOf('other')]; // Medio hardcodeado
 		$scope.product.images = new Array(productImagesCount);
-        $scope.product.videoLinks = new Array(productVideosCount);
+        $scope.product.videos = new Array(productVideosCount);
 				
 		$scope.doSubmit = function() {
 			var empty = true;
@@ -17,17 +17,21 @@ define(['productSeek', 'directives/ngFileRead'], function(productSeek) {
 				if (img)
 					empty = false;
 			});
-			angular.forEach($scope.product.videoLinks, function(video) {
+			angular.forEach($scope.product.videos, function(video) {
 				if (video)
 					empty = false;
 			});
 			$scope.noImagesError = empty;
 			
-			console.log($scope.product);
+			restService.postProduct($scope.product);
 		};
 		
 		$scope.deleteImage = function(index) {
 			$scope.product.images[index] = null;
 		};
+		
+		$scope.deleteLogo = function() {
+			$scope.product.logo = null;
+		}
 	}]);
 });
