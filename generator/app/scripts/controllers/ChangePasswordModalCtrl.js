458d9596755644c01e6beeaf9e76c47ab7160ca4
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2017-10-17 11:15:54 -0300

    Cambiar contraseña sin validaciones

--- generator/app/scripts/controllers/ChangePasswordModalCtrl.js
+++ generator/app/scripts/controllers/ChangePasswordModalCtrl.js
@@ -1,28 +1,35 @@
 define(['productSeek', 'services/authService', 'services/sessionService', 'services/restService'], function(productSeek) {
 
     'use strict';
-    productSeek.controller('ChangePasswordModalCtrl', ['authService', 'sessionService', 'restService', '$scope', function(auth,session,restService, $scope) {
+    productSeek.controller('ChangePasswordModalCtrl', ['authService', 'sessionService', 'restService', '$scope', '$uibModalInstance', function(auth,session,restService, $scope, $modal) {
         $scope.passwordForm = {};
         $scope.passwordForm.oldPassword = {};
         $scope.passwordForm.newPassword = {};
         $scope.passwordForm.confirmPassword = {};
         
-        $scope.passwordSubmit = function() {
-            console.log($scope.passwordForm);
-            //Falta validación de newPassword = confirmPassword
-            oldPass = $scope.passwordForm.oldPassword.text;
-            newPass = $scope.passwordForm.newPassword.text;
-            confirm = $scope.passwordForm.confirmPassword.text;
-            if(newPass === confirm){
-                restService.changePassword(oldPass,newPass);
-            } else {
-                //Validaciones en caso de que las contraseñas ingresadas sean distintas
-            }
-        };
-        
         $scope.cancel = function(){
             $scope.$dismiss();
         };
+
+        $scope.passwordSubmit = function() {
+            console.log($scope.passwordForm);
+            
+            if($scope.passwordForm.newPassword.text === $scope.passwordForm.confirmPassword.text) {
+                console.log("Son iguales");
+                restService.changePassword($scope.passwordForm.oldPassword.text,$scope.passwordForm.newPassword.text)
+                .then(function(response) {
+                    alert('Password Changed')
+                    $modal.dismiss();
+                })
+                .catch(function(response) {
+                    alert('La contraseña actual no es la correcta');
+                    console.log(response);
+                });
+            } else {
+                    console.log("Son distintas");
+                };	
+		};
+
     }]);
 
 });
